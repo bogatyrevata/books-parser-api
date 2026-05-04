6963255eea7d8408de6403f349f5755555359e40
taisiia bogatyreva <bogatyrevata@gmail.com>
2026-05-03 21:28:09 -0300

    fix tests, async rewrite complete

--- parser/main.py
+++ parser/main.py
@@ -1,11 +1,14 @@
-import time, os, csv
+import asyncio
+import os
+import csv
 from dotenv import load_dotenv
 from urllib.parse import urljoin
-
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy import select
 from database import Book, Base, get_engine
-from sqlalchemy.orm import Session
 from loguru import logger
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
+
 
 logger.add("logs/parser.log", 
            rotation="1 MB",       # новый файл когда вырастет до 1MB
@@ -14,89 +17,131 @@ logger.add("logs/parser.log",
 
 load_dotenv()
 
-# Создаёт таблицу в БД если её ещё нет
-engine = get_engine()
-Base.metadata.create_all(engine)
-
-all_books = []
 LIMIT_BOOKS = int(os.getenv("LIMIT_BOOKS")) # Получаем лимит из переменных окружения
-
 current_url = os.getenv("CATEGORY_URL")
 rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)#открывает окно браузера, false чтобы видеть процесс, true для работы в фоне
-    page = browser.new_page() #открывает новую вкладку
+all_books = []
 
-    page.goto(current_url)#переходит по URL категории книг
-    while len(all_books) < LIMIT_BOOKS:
-        # Ждём карточки книг
-        try:            
-            page.wait_for_selector("article.product_pod")#ждём пока на странице появится элемент с классом product_pod, который соответствует карточке книги. Это гарантирует, что страница полностью загрузилась и мы можем безопасно извлекать данные о книгах.
+async def parse_books() -> list[dict]:
+    '''Парсит книги с сайта и возвращает список словарей с данными о книгах.
+      Каждая книга представлена словарём с ключами: title, url, price, in_stock, rating. 
+      Функция использует Playwright для асинхронного взаимодействия с сайтом. 
+      Она открывает браузер, переходит по URL категории книг и извлекает данные о каждой книге на странице. 
+      Если количество собранных книг достигает LIMIT_BOOKS, парсинг прекращается. 
+      Если на странице есть кнопка "next", функция кликает по ней и продолжает парсинг следующей страницы. 
+      В случае ошибок при парсинге страницы, ошибка логируется и парсинг прекращается.'''
+    all_books = []
+    url = current_url
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)#открывает окно браузера, false чтобы видеть процесс, true для работы в фоне
+        page = await browser.new_page() #открывает новую вкладку
+        await page.goto(url)#переходит по URL категории книг
 
-            # Берём все карточки
-            books = page.query_selector_all("article.product_pod")
+        while len(all_books) < LIMIT_BOOKS:
+            # Ждём карточки книг
+            try:            
+                await page.wait_for_selector("article.product_pod")#ждём пока на странице появится элемент с классом product_pod, который соответствует карточке книги. Это гарантирует, что страница полностью загрузилась и мы можем безопасно извлекать данные о книгах.
 
+                # Берём все карточки
+                books = await page.query_selector_all("article.product_pod")
+
+                for book in books:
+                    if len(all_books) >= LIMIT_BOOKS:
+                        break
+
+                    title = await (await book.query_selector("h3 a")).get_attribute("title")
+                    relative_url = await (await book.query_selector("h3 a")).get_attribute("href")
+                    book_url = urljoin(url, relative_url)
+                    price_text = await (await book.query_selector("p.price_color")).inner_text()
+                    price = float(price_text.replace('£', '').strip())
+                    stock_text = await (await book.query_selector("p.instock.availability")).inner_text()
+                    in_stock = 'In stock' in stock_text
+                    rating_class = await (await book.query_selector("p.star-rating")).get_attribute("class")
+                    rating_name = rating_class.split()[1]
+                    rating = rating_map.get(rating_name, 0)
+
+                    all_books.append({
+                        "title": title, 
+                        "url": book_url, 
+                        "price": price, 
+                        "in_stock": in_stock, 
+                        "rating": rating
+                        })
+                    
+                    logger.info(f"{len(all_books)}. {title} | {price} | {rating} | {in_stock}")
+                    await asyncio.sleep(1)  # asyncio.sleep позволяет приостановить выполнение текущей задачи на заданное количество секунд, не блокируя при этом выполнение других задач. Это особенно полезно в асинхронных программах, где нужно выполнять несколько операций одновременно, например, обрабатывать данные и делать запросы к серверу. В данном случае, после обработки каждой книги, мы делаем паузу в 1 секунду, чтобы не перегружать сайт слишком быстрыми запросами и имитировать более естественное поведение пользователя.
+            
+            except Exception as e:
+                logger.error(f"Ошибка при парсинге страницы {url}: {e}")
+                break   
+
+            # Ищем кнопку next
+            next_btn = await page.query_selector("li.next a")
+            if next_btn:
+                await next_btn.click()
+                await page.wait_for_load_state("networkidle")
+            else:
+                break
+
+        await browser.close()
+
+    return all_books
+
+
+async def save_to_csv(books: list[dict]) -> None:
+    '''Сохраняет список книг в CSV файл. Каждая книга представлена словарём с ключами:
+      title, url, price, in_stock, rating. Файл сохраняется с именем "books.csv" в 
+      кодировке UTF-8. В первой строке файла записываются заголовки столбцов. 
+      Каждая последующая строка соответствует одной книге из списка.'''
+    with open("books.csv", "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ["title", "url", "price", "in_stock", "rating"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for book in books:
+            writer.writerow(book)
+    logger.info(f"Сохранено в CSV: {len(books)} книг")
+
+
+async def save_to_db(books: list[dict], engine) -> None:
+    '''Сохраняет список книг в базу данных. Каждая книга представлена словарём с ключами:
+      title, url, price, in_stock, rating. Функция использует SQLAlchemy для взаимодействия с базой данных. 
+      Для каждой книги проверяется наличие записи с таким же URL. Если запись с таким URL уже существует, книга пропускается и логируется предупреждение. 
+      Если книги с таким URL нет, она добавляется в базу данных. В конце функции логируется количество сохранённых книг и количество пропущенных из-за дублей. 
+      В случае ошибок при сохранении в базу данных, ошибка логируется как критическая.'''
+    AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+    try:
+        async with AsyncSessionLocal() as db:
+            counter = 0 # Счётчик сохранённых книг
+            skipped = 0 # Счётчик пропущенных книг из-за дублей
             for book in books:
-                if len(all_books) >= LIMIT_BOOKS:
-                    break
-                title = book.query_selector("h3 a").get_attribute("title")
-                relative_url = book.query_selector("h3 a").get_attribute("href") # Получаем относительный URL так как в playwright нет метода для получения абсолютного URL
-                url = urljoin(current_url, relative_url) # склеиваем базовый URL с относительным, чтобы получить полный URL книги
-                price = float(book.query_selector("p.price_color").inner_text().replace('£', '').strip())
-                in_stock = 'In stock' in book.query_selector("p.instock.availability").inner_text() # Проверяем наличие книги в наличии, если в тексте элемента с классом instock.availability есть фраза 'In stock', то считаем, что книга есть в наличии
-                rating_name = book.query_selector("p.star-rating").get_attribute("class").split()[1]  # получаем класс и разбиваем его по пробелу на части, чтобы извлечь название рейтинга (One, Two, Three, Four, Five)
-                rating = rating_map.get(rating_name, 0)
-                all_books.append({
-                    "title": title, 
-                    "url": url, 
-                    "price": price, 
-                    "in_stock": in_stock, 
-                    "rating": rating
-                    })
-                
-                logger.info(f"{len(all_books)}. {title} | {price} | {rating} | {in_stock}")
-                time.sleep(1)  # пауза между запросами
-        except Exception as e:
-            logger.error(f"Ошибка при парсинге страницы {current_url}: {e}")
-            break   
+                result = await db.execute(select(Book).where(Book.url == book['url'])) # Проверяем, есть ли уже книга с таким URL в БД
+                exists = result.scalar_one_or_none()
+                if not exists:
+                    db.add(Book(**book)) # Если книги с таким URL нет, добавляем её в БД
+                    counter += 1
+                else:
+                    skipped += 1
+                    logger.warning(f"Уже в БД: {book['title']}")
+            await db.commit()
+        logger.info(f"Сохранено в БД: {counter} книг | Пропущено дублей: {skipped}")
+    except Exception as e:
+        logger.critical(f"Ошибка сохранения в БД: {e}")
 
-        # Ищем кнопку next
-        next_btn = page.query_selector("li.next a")
-        if next_btn:
-            next_btn.click()
-            page.wait_for_load_state("networkidle")
-        else:
-            break
+async def main():
+    # создаём таблицы если нет
+    engine = get_engine() # Получаем асинхронный движок базы данных, который будет использоваться для взаимодействия с базой данных. Этот движок создаётся с помощью функции get_engine(), которая обычно настраивается для подключения к конкретной базе данных, например, PostgreSQL.
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
-    browser.close()
+    # парсим
+    books = await parse_books()
+    logger.info(f"Всего книг: {len(books)}")
 
-# Вывести все собранные книги и их URL
-logger.info(f"Всего книг: {len(all_books)}")
-
-# Сохранить результаты в файл csv
-with open("books.csv", "w", newline="", encoding="utf-8") as csvfile:
-    fieldnames = ["title", "url", "price", "in_stock", "rating"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for book in all_books:
-        writer.writerow(book)
+    # сохраняем
+    await save_to_csv(books)
+    await save_to_db(books, engine)
 
 
-# Сохранить результаты в базу данных
-try:
-     with Session(engine) as db:
-         counter = 0 # Счётчик сохранённых книг
-         skipped = 0 # Счётчик пропущенных книг из-за дублей
-         for book in all_books:
-             exists = db.query(Book).filter_by(url=book['url']).first() # Проверяем, есть ли уже книга с таким URL в БД
-             if not exists:
-                 db.add(Book(**book)) # Если книги с таким URL нет, добавляем её в БД
-                 counter += 1
-             else:
-                 skipped += 1
-                 logger.warning(f"Уже в БД: {book['title']}")
-         db.commit()
-     logger.info(f"Сохранено в БД: {counter} книг | Пропущено дублей: {skipped}")
-except Exception as e:
-     logger.critical(f"Ошибка сохранения в БД: {e}")
+if __name__ == "__main__":
+    asyncio.run(main())
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,33 @@
 # tests/conftest.py
 import pytest
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from fastapi.testclient import TestClient
 from database import Base
-from api.api import app, get_db
+from api.api import app
+from api.dependencies import get_db
 
 TEST_DB = "test.db"
 
-engine = create_engine(
-    f"sqlite:///{TEST_DB}",
+engine = create_async_engine(
+    f"sqlite+aiosqlite:///{TEST_DB}",
     connect_args={"check_same_thread": False}
 )
 
-TestingSessionLocal = sessionmaker(bind=engine)
+AsyncTestingSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 @pytest.fixture(autouse=True)
-def setup_db():
-    Base.metadata.create_all(engine)
+async def setup_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     yield
-    Base.metadata.drop_all(engine)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
 @pytest.fixture
 def client():
-    def override_get_db():
-        with TestingSessionLocal() as session:
+    async def override_get_db():
+        async with AsyncTestingSessionLocal() as session:
             yield session
 
     app.dependency_overrides[get_db] = override_get_db
@@ -34,19 +36,15 @@ def client():
 
 @pytest.fixture
 def admin_client(client):
-    # регистрируем первого пользователя — он станет админом
     client.post("/auth/register", json={
         "username": "admin",
         "email": "admin@test.com",
         "password": "12345"
     })
-    # логинимся и получаем токен
     response = client.post("/auth/login", data={
         "username": "admin",
         "password": "12345"
     })
     token = response.json()["access_token"]
-
-    # добавляем токен в заголовки клиента
     client.headers.update({"Authorization": f"Bearer {token}"})
     return client
